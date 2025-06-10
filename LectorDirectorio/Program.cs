5281bdbb4a43bc4c21822f09012c9da29fa22256
ivanncomit <fabrivancj@gmail.com>
2025-06-10 13:30:31 -0300

    Correccion errores

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -10,7 +10,7 @@ do
 {
     pathIndicado = Console.ReadLine();
 
-    if(!Directory.Exists(pathIndicado))
+    if (!Directory.Exists(pathIndicado))
     {
         Console.WriteLine("Ingrese un PATH valido: ");
     }
@@ -18,40 +18,33 @@ do
 } while (!Directory.Exists(pathIndicado));
 
 var filesPathIndicado = Directory.GetFiles(pathIndicado);
-if(filesPathIndicado==null)
+if (filesPathIndicado == null)
 {
     System.Console.WriteLine("\nEl Directorio no tiene archivos\n");
-} else 
+}
+else
 {
     System.Console.WriteLine("\nEl Directorio tiene los archivos:\n");
-    foreach(var archivo in filesPathIndicado)
+    foreach (var archivo in filesPathIndicado)
     {
         var aux = new FileInfo(archivo);
-        Console.WriteLine($"Archivo: {Path.GetFileName(archivo)} - Peso: {aux.Length/1024}KB");
-    }  
+        Console.WriteLine($"Archivo: {Path.GetFileName(archivo)} - Peso: {aux.Length / 1024}KB");
+    }
 }
-
 
 directorios = Directory.GetDirectories(pathIndicado);
 
 Console.WriteLine("\nCarpetas en el Path Indicado:");
 
-foreach(var carpetas in directorios)
+foreach (var carpetas in directorios)
 {
     archivos = Directory.GetFiles(carpetas);
     var carpeta = carpetas.Split("\\");
-    Console.WriteLine($"\n-------Archivos de la Carpeta: {carpeta[carpeta.Length-1]}-------");
-    foreach(var archivo in archivos)
+    Console.WriteLine($"\n-------Archivos de la Carpeta: {carpeta[carpeta.Length - 1]}-------");
+    foreach (var archivo in archivos)
     {
-        /*         var salida = Path.GetFileName(archivo); */
-
-        /* string[] salida = archivo.Split('\\');
-        foreach (var item in salida)
-        {
-            System.Console.WriteLine(item);
-         */
         var aux = new FileInfo(archivo);
-        Console.WriteLine($"Archivo: {Path.GetFileName(archivo)} - Peso: {aux.Length/1000}KB");
+        Console.WriteLine($"Archivo: {Path.GetFileName(archivo)} - Peso: {aux.Length / 1000}KB");
     }
 
 }
